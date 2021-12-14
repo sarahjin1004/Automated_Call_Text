3c82875d840c4c1287fb7edf182916358207dec9
Sarah Jin <sjin@sjin-mac.local>
2021-12-13 20:57:28 -0800

    send patients english and translated text message.

--- send_sms.py
+++ send_sms.py
@@ -3,7 +3,7 @@ import os
 from twilio.rest import Client
 import translate
 
-translated_message = translate.translate_text("ko", "hello")
+#translated_message = translate.translate_text("ko", "hello")
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
@@ -11,11 +11,12 @@ account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
-message = client.messages \
-                .create(
-                     body=translated_message,
-                     from_=os.environ['TWILIO_PHONE'],
-                     to=os.environ['RECEIVER_PHONE']
-                 )
+def send_text(message, caller):
+    message = client.messages \
+                    .create(
+                        body=message,
+                        from_=os.environ['TWILIO_PHONE'],
+                        to=caller
+                    )
 
-print(message.sid)
+#send_text()
--- spreadsheet.py
+++ spreadsheet.py
@@ -1,18 +1,70 @@
 
 ''' ###################################
--- service_account.json is in ~/.config/gspread
 -- if service_account.json is in project directory, use:
 sa = gspread.service_account(filename='service_account.json')
 ###################################'''
 
 import gspread
+import pandas as pd
+import translate
+import send_sms
 
-sa = gspread.service_account(filename='service_account.json')
+#open the document
+sa = gspread.service_account()
 sh = sa.open("AutomatedSystem TestSheet")
-
 wks = sh.worksheet("Sheet1")
+d = wks.get_all_records()
+df = pd.DataFrame(data=d)
 
-print(wks.acell('C2').value)
-print(wks.get_all_records())
-wks.update('A2', 'no show')
+#global variables
+col_to_letter = {}
+patient_data = {}
+
+#print(df.columns)
+def add_to_dict():
+    starting_col = 65
+    for c in df.columns:
+        col_to_letter[c] = chr(starting_col)
+        starting_col += 1
+add_to_dict()
+
+def add_to_patient_data():
+    i = 2
+    for data in d:
+        patient_data[i] = data
+        i += 1
+add_to_patient_data()
+
+for id in patient_data:
+    name = patient_data[id]['Patient Name (Last, first)']
+    message = patient_data[id]["Message"]
+    language = patient_data[id]["Language"]
+    phone = str(patient_data[id]["Phone #"])
+    lang_code = translate.languages(language)
+    translated = translate.translate_text(lang_code, message)
+    wks.update(col_to_letter["Translated Message"]+str(id), translated)
+    patient_data[id]["Translated Message"] = translated
+    #print(name, message, "+1"+phone)
+
+for i in patient_data:
+    unique_id = i
+    name = patient_data[unique_id]["Patient Name (Last, first)"].split((", "))
+    first, last = name[1], name[0]
+    m = patient_data[unique_id]["Message"]
+    daytime = patient_data[unique_id]["Day/Time of Scheduling"]
+    appointment_time = patient_data[unique_id]["Appointment Time"]
+    mess = "Hello "+first+" "+last+", \n"+"You have an appointment at Paul Hom Asian Clinic on "+daytime+" "+appointment_time+". Call 4088052770 to CANCEL or RESCHEDULE. \n"
+    language = patient_data[unique_id]["Language"]
+    lang_code = translate.languages(language)
+    tr_mess = translate.translate_text(lang_code, mess)
+    p = patient_data[unique_id]["Phone #"]
+    mess = mess+"************\n"+tr_mess
+    print(mess)
+    send_sms.send_text(mess, p)
+
+
+
+#df = pd.DataFrame(data = d,columns=["Appointment Time", "Language", "Phone #"])
+#print(df)
+#wks.update('A2', 'need to reschedule')
 
